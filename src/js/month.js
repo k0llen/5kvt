458312322f34456432dd4fd4c91fa04b9a99d766
Nikolay Yatsenko <yatsenkokolyan@gmail.com>
2023-04-17 18:27:44 +0600

    Fixes + promo section + old sections adaptive

--- src/js/month.js
+++ src/js/month.js
@@ -5,9 +5,23 @@ if (document.querySelector('.month')) {
             prevEl: '.month__swiper-button-prev'
         },
         loop: true,
-        speed: 600,
-        slidesperView: 4,
+        speed: 500,
+        slidesPerView: 4,
         slidesToScroll: 1,
         spaceBetween: 30,
+        breakpoints: {
+            300 : {
+                loop: false,
+                slidesPerView: 2,
+                spaceBetween: 15,
+                grid: {
+                    fill: 'row',
+                    rows: 2,
+                },
+            },
+            1000: {
+                slidesPerView: 4,
+            }
+        },
     })
 }
