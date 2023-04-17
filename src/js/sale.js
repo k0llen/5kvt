442bc5680f32a1036f6062766d6fd8a37d371423
Nikolay Yatsenko <yatsenkokolyan@gmail.com>
2023-04-17 18:49:54 +0600

    Sale section + adaptive + fixes

---
+++ src/js/sale.js
@@ -0,0 +1,28 @@
+if (document.querySelector('.sale')) {
+    const monthSwiper = new Swiper('.sale__swiper', {
+        navigation: {
+            nextEl: '.sale__swiper-button-next',
+            prevEl: '.sale__swiper-button-prev'
+        },
+        loop: true,
+        speed: 500,
+        slidesPerView: 4,
+        slidesToScroll: 1,
+        spaceBetween: 30,
+        breakpoints: {
+            300 : {
+                loop: false,
+                slidesPerView: 2,
+                spaceBetween: 15,
+                grid: {
+                    fill: 'row',
+                    rows: 2,
+                },
+            },
+            1000: {
+                slidesPerView: 4,
+
+            }
+        },
+    })
+}
