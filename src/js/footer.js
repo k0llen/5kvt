74771b561619d442977623e4ff081d1e8d4b8f48
Nikolay Yatsenko <yatsenkokolyan@gmail.com>
2023-04-18 15:52:49 +0600

    Footer + adaptive

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,42 +1,19 @@
-// (function() {
-//   if (document.querySelectorAll(".work-card"))
-//     document.querySelectorAll(".work-card").forEach((el) => {
-//       el.addEventListener('mousemove', (e) => {
-//         const rect = e.target.getBoundingClientRect()
-//         let x = e.clientX - rect.left
-//         let y = e.clientY - rect.top
-//         let rightBorder = el.offsetWidth - 60
-//         let bottomBorder = el.offsetHeight - 60
-
-//         if (x < 60) {
-//           x = 60
-//         }
-
-//         if (x > rightBorder) {
-//           x = rightBorder
-//         }
-
-//         if (y < 60) {
-//           y = 60
-//         }
-
-//         if (y > bottomBorder) {
-//           y = bottomBorder
-//         }
-
-//         gsap.to(el.children[2], {
-//           opacity: 1,
-//           x,
-//           y,
-//           duration: 0.3
-//         })
-//       })
-
-//       el.addEventListener('mouseleave', (e) => {
-//         gsap.to(el.children[2], {
-//           opacity: 0,
-//           duration: 0.3
-//         })
-//       })
-//     })
-// })();
+if (document.querySelector('.footer')) {
+    if (window.innerWidth <= 1003) {
+        const accordeonTrigger = document.querySelectorAll('.footer__block-2-list-trigger');
+        
+        accordeonTrigger.forEach((item) => {
+            item.addEventListener('click', function () {
+                if (item.parentNode.classList.contains('footer__accordeon--active')) {
+                    item.parentNode.classList.remove('footer__accordeon--active');
+                } else {
+                    accordeonTrigger.forEach((child) => {
+                        child.parentNode.classList.remove('footer__accordeon--active');
+                    })
+                    item.parentNode.classList.add('footer__accordeon--active');
+                };
+            });
+        })
+    
+    }
+}
