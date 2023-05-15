8da85935fce5291621b39b7a6ba826fc6fea471c
Nikolay Yatsenko <yatsenkokolyan@gmail.com>
2023-05-15 12:54:25 +0600

    Fixes + smooth scroll

--- src/js/header.js
+++ src/js/header.js
@@ -218,5 +218,19 @@ if (document.querySelector('.header__catalog')) {
         });
     });
 
+    // Smooth scroll
+
+    const anchors = document.querySelectorAll('a[href*="#"]');
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const blockID = anchor.getAttribute('href');
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    };
 
 }
